fb0a070588b79fd81f1d78737404520ecd0bf900
DmitriyDS <70814879+DmitriyDS91@users.noreply.github.com>
2020-10-20 01:10:44 +0500

    feat: add solution for viginere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(typeOfEncryptionMachine) {
+    this.typeOfEncryptionMachine = typeOfEncryptionMachine;
+  }
+
+  encrypt(massage, key) {
+    let spaceIndex = 0;
+    let result = '';
+    let a = '';
+    massage = massage.toLowerCase();
+    key = key.toLowerCase();
+
+    for (i = 0; i < massage.length; i++) {
+      if (/[a-z]/.test(massage[i]) === true) {
+        a = String.fromCharCode((massage[i].charCodeAt() - 97 + key[(i - spaceIndex) % key.length].charCodeAt() - 97) % 26 + 97);
+        result = `${result}${a}`
+      } else {
+        result = `${result}${massage[i]}`;
+        spaceIndex++;
+      }
+    }
+
+    if (this.typeOfEncryptionMachine === false) {
+      return result.split('').reverse().join('').toUpperCase();
+    } else {
+      return result.toUpperCase();
+    }
+  }   
+
+  decrypt(massage, key) {
+    let spaceIndex = 0;
+    let result = '';
+    let a = '';
+    massage = massage.toLowerCase();
+    key = key.toLowerCase();
+
+    for (i = 0; i < massage.length; i++) {
+      if (/[a-z]/.test(massage[i]) === true) {
+        a = String.fromCharCode((massage[i].charCodeAt() - 97 - (key[(i - spaceIndex) % key.length].charCodeAt() - 97) + 26) % 26 + 97);
+        result = `${result}${a}`
+      } else {
+        result = `${result}${massage[i]}`;
+        spaceIndex++;
+      }
+    }
+
+    if (this.typeOfEncryptionMachine === false) {
+      return result.split('').reverse().join('').toUpperCase();
+    } else {
+      return result.toUpperCase();
+    }
   }
 }
 
