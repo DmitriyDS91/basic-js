f2021ef480bcfee842b47ac6fdaf509c3a6477e4
DmitriyDS <70814879+DmitriyDS91@users.noreply.github.com>
2020-10-20 01:09:18 +0500

    feat: add solution for transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+ let newArray = arr.slice(0);
+  if (Object.prototype.toString.call(arr) !== '[object Array]') throw new Error ();
+  if (arr == []) return [];
+  
+
+  for (i = 0; i < newArray.length; i++) {
+    switch (newArray[i]) {
+      case '--discard-next':
+        if (i + 1 < newArray.length) {
+          newArray.splice(i + 1, 1, 'discarded')
+          newArray.splice(i, 1);
+          i = i - 1;
+          discarded = true;
+        } else {
+          newArray.splice(i, 1);
+        }
+        break;
+      case '--discard-prev':
+        if (i >= 1 && newArray[i - 1] != 'discarded') {
+          newArray.splice(i - 1, 2);
+          i = i - 2;
+        } else {
+          newArray.splice(i, 1);
+        }
+        break;
+      case '--double-next':
+        if (i + 1 < newArray.length) {
+          newArray.splice(i, 1, newArray[i + 1]);
+        } else {
+          newArray.splice(i, 1);
+        }
+        break;
+      case '--double-prev':
+        if (i >= 1 && newArray[i - 1] != 'discarded') {
+          newArray.splice(i, 1, newArray[i - 1]);
+        } else {
+          newArray.splice(i, 1);
+        }
+        break;
+    }
+  }
+
+  for (i = 0; i < newArray.length; i++) {
+    if (newArray[i] == 'discarded') {
+      newArray.splice(i, 1);
+      i = i - 1;
+    }
+  }
+
+  return newArray;
 };
